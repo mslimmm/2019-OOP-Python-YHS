0175dbd69ea5a94f45dc6cf435d556d4785280d4
임민서 <ms_lim@sasa.hs.kr>
2019-11-16 16:36:46 +0900

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,54 @@
-print(1)
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode([640, 480])
+size = [640, 480]
+pygame.display.set_caption("YHS")
+
+win.fill((255, 255, 255))
+
+
+def coord(loc):
+    return [size[0]/2+loc[0], size[1]/2-loc[1]]
+
+
+class Node:
+    def __init__(self, loc):
+        self.next = []
+        self.x, self.y = coord(loc)
+
+    def draw(self):
+        pygame.draw.circle(win, (0, 0, 0), (self.x, self.y), 10, 1)
+        pygame.display.flip()
+
+
+start = Node([300, -300])
+done = False
+
+while not done:
+    pygame.time.delay(1000)
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True
+    start.draw()
+
+pygame.quit()
+
+"""
+(2,0)   (1,4)   (1,3)   (1,2)   (1,1)   (1,0)
+
+
+(2,1)   (-2,1)                 (-1,1)   (0,4)
+
+
+(2,2)       (-2,2)         (-1,2)       (0,3)
+                   (-1,3)
+                   (-2,3)
+(2,3)       (-1,4)         (-2,4)       (0,2)
+
+
+(2,4)   (-1,5)                 (-2,5)   (0,1)
+
+(3,0)   (3,1)   (3,2)   (3,3)   (3,4)   (0,0) (4,0)
+"""
---
+++ 2.py
@@ -0,0 +1,74 @@
+"""
+(2,0)   (1,4)   (1,3)   (1,2)   (1,1)   (1,0)
+
+
+(2,1)   (-2,1)                 (-1,1)   (0,4)
+
+
+(2,2)       (-2,2)         (-1,2)       (0,3)
+                   (-1,3)
+                   (-2,3)
+(2,3)       (-1,4)         (-2,4)       (0,2)
+
+
+(2,4)   (-1,5)                 (-2,5)   (0,1)
+
+(3,0)   (3,1)   (3,2)   (3,3)   (3,4)   (0,0) (4,0)
+"""
+import pygame, sys, random
+from pygame.locals import *
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '0'
+
+pygame.init()
+pygame.display.set_caption("YUT")
+screen_x = 800
+screen_y = 600
+board_len = screen_y - 180
+screen = pygame.display.set_mode((screen_x, screen_y))
+clock = pygame.time.Clock()
+
+sp_image = pygame.image.load("images/small_point.png")
+bp_image = pygame.image.load("images/big_point.png")
+def find_loc(x, y):
+    if x == 0:
+        return (board_len / 2, -board_len / 2 + board_len * y / 5)
+    if x == 1:
+        return (board_len / 2 - board_len * y / 5, board_len / 2)
+    if x == 2:
+        return (-board_len / 2, board_len / 2 - board_len * y / 5)
+    if x == 3:
+        return (-board_len / 2 + board_len * y / 5, -board_len / 2)
+    if x == -1:
+        return (board_len / 2 - board_len * y / 6, board_len / 2 - board_len * y / 6)
+    if x == -2:
+        return (-board_len / 2 + board_len * y / 6, board_len / 2 - board_len * y / 6)
+
+def blit_center(image, tuple):
+    x, y = tuple
+    xsize, ysize = image.get_rect().size
+    screen.blit(image, (x-xsize/2, y-ysize/2))
+
+def find_coord(tuple):
+    x, y = tuple
+    return (x + screen_x / 2, y + screen_y / 2)
+
+screen.fill((255, 255, 255))
+
+for i in range(0, 4):
+    for j in range(0, 5):
+        image = sp_image
+        if j == 0:
+            image = bp_image
+        blit_center(image, find_coord(find_loc(i, j)))
+
+for i in range (-1, -3, -1):
+    for j in range(1, 6):
+        image = sp_image
+        if j == 3:
+            image = bp_image
+        blit_center(image, find_coord(find_loc(i, j)))
+
+while True:
+    pygame.display.update()
